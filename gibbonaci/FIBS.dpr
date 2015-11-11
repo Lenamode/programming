e37eb5a4105f2edc43bdcfda08e61168504c52c3
Sixx <snowball2k10@hotmail.co.uk>
2015-11-11 11:58:59 +0000

    Fibbing
    Added the fibbonaci code, which finally works. Need to add the
    executable, though.

---
+++ gibbonaci/FIBS.dpr
@@ -0,0 +1,29 @@
+program FIBS;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+function fib(n:integer): integer;
+begin
+    if (n <= 2) then
+        fib := 1
+    else
+        fib := fib(n-1) + fib(n-2);
+end;
+
+var
+    i, iterations:integer;
+
+begin
+writeln('Hey you, this is a program written for @duck over at C16.');
+writeln('How many fibbonaci numbers would you like to print?');
+readln(iterations);
+    for i := 1 to iterations do
+        write(fib(i), ', ');
+    writeln('...');
+    readln;
+end.
